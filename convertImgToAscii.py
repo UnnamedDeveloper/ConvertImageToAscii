36392e948fcaa84ec4516e807ac5e43879558242
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2020-09-12 19:07:17 +0200

    Add pixel-by-pixel reading

--- convertImgToAscii.py
+++ convertImgToAscii.py
@@ -1,5 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import sys
 
-image = Image.open(sys.argv[1])
-image.show();
+# Load image
+raw_image = Image.open(sys.argv[1])
+raw_image_pixels = raw_image.load()
+raw_image_width, raw_image_height = raw_image.size
+
+print("Image Data:")
+print(f"  Path: {sys.argv[1]}")
+print(f"  Width: {raw_image_width}")
+print(f"  Height: {raw_image_height}")
+print(f"    (size: {raw_image.size})")
+
+# Loop over image pixels
+for y in range(raw_image_height):
+    for x in range(raw_image_width):
+        print(x * y + x, end="")
+
+    print() # Newline
