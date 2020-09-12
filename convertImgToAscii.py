d4b27ea8185f15bf65d768957c294b63b039ad53
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2020-09-12 19:49:46 +0200

    Formatted and documented code

--- convertImgToAscii.py
+++ convertImgToAscii.py
@@ -2,19 +2,44 @@ from PIL import Image, ImageFilter
 from argparse import ArgumentParser
 import sys
 
+
 def get_pixel_color_space_luminance(pixel):
+    '''Get the pixels luminance
+
+    Get the luminance of the given pixel using the color
+    space luminance function.
+    '''
     red, green, blue = pixel
     return (0.2126 * red + 0.7152 * green + 0.0722 * blue)
 
+
 def get_pixel_perceived_luminance(pixel):
+    '''Get the pixels luminance
+
+    Get the luminance of the given pixel using the percieved
+    luminance function.
+    '''
     red, green, blue = pixel
     return (0.299 * red + 0.587 * green + 0.114 * blue)
 
+
 def get_pixel_perceived_sqrt_luminance(pixel):
+    '''Get the pixels luminance
+
+    Get the luminance of the given pixel using the perceived_sqrt
+    luminance function.
+    '''
     red, green, blue = pixel
     return sqrt(0.299 * red^2 + 0.587 * green^2 + 0.114 * blue^2)
 
+
 def get_pixel_luminance(luminance_func, pixel):
+    '''Get the luminance of a pixel
+
+    Get the luminance of the given pixel using the given luminance function
+    from the input arguments to convert the pixel.
+    '''
+
     if luminance_func == "color_space":
         return get_pixel_color_space_luminance(pixel)
     elif luminance_func == "perceived":
@@ -25,40 +50,55 @@ def get_pixel_luminance(luminance_func, pixel):
         print("ERROR: Cannot recognice given luminance function argument")
         sys.exit(-1)
 
+
 def get_pixel_luminance_1_0(luminance_func, pixel, max_pixel):
+    '''Get the luminance of a pixel as a value between 1 and 0
+    
+    Use the given luminance function from the input arguments to get
+    the luminance of the given pixel, and then give it a value between
+    1 and 0 where 0 is no luminance and 1 is the luminance of the
+    brightest possible pixel.
+    '''
     luminance = get_pixel_luminance(luminance_func, pixel)
     max_luminance = get_pixel_luminance(luminance_func, max_pixel)
 
     return luminance / max_luminance
 
-# Create argument parser
-parser = ArgumentParser(description="Convert a standart image into ascii art of said image.")
 
-# Required arguments
-parser.add_argument("filepath", type=str, help="The path to the input file")
+if __name__ == "__main__":
 
-# Optional arguments
-parser.add_argument("-o", type=str, default="out", help="Path to output file")
-parser.add_argument("--luminance-func", choices=["color_space", "perceived", "perceived_sqrt"], help="Which function to use to calculate luminance. Can be: 'color_space', 'percieved', 'perceived_sqrt'")
+    # Create argument parser
+    parser = ArgumentParser(description="Convert a standart image into ascii art of said image.")
 
-# Parse arguments
-args = parser.parse_args()
+    # Required arguments
+    parser.add_argument("filepath", type=str, help="The path to the input file")
 
-# Load image
-raw_image = Image.open(sys.argv[1])
-raw_image_pixels = raw_image.load()
-raw_image_width, raw_image_height = raw_image.size
+    # Optional arguments
+    parser.add_argument("-o", type=str, default="out", help="Path to output file")
+    parser.add_argument("--luminance-func", choices=["color_space", "perceived", "perceived_sqrt"], help="Which function to use to calculate luminance. Can be: 'color_space', 'percieved', 'perceived_sqrt'")
 
-print("Image Data:")
-print(f"  Path: {sys.argv[1]}")
-print(f"  Width: {raw_image_width}")
-print(f"  Height: {raw_image_height}")
-print(f"    (size: {raw_image.size})")
+    # Parse arguments
+    args = parser.parse_args()
 
-# Loop over image pixels
-for y in range(raw_image_height):
-    for x in range(raw_image_width):
-        pixel_luminance = get_pixel_luminance_1_0(args.luminance_func, raw_image_pixels[x, y], [255, 255, 255])
-        print(pixel_luminance, end=" ")
+    # Load image
+    raw_image = Image.open(sys.argv[1])
+    raw_image_pixels = raw_image.load()
+    raw_image_width, raw_image_height = raw_image.size
 
-    print() # Newline
+    # Log image data
+    print("Image Data:")
+    print(f"  Path: {sys.argv[1]}")
+    print(f"  Width: {raw_image_width}")
+    print(f"  Height: {raw_image_height}")
+    print(f"    (size: {raw_image.size})")
+    
+    # Loop over image pixels
+    for y in range(raw_image_height):
+        for x in range(raw_image_width):
+            # Get luminance of pixel
+            pixel_luminance = get_pixel_luminance_1_0(args.luminance_func, raw_image_pixels[x, y], [255, 255, 255])
+
+            print(pixel_luminance, end=" ")
+    
+        print() # Newline
+
